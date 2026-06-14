20a3d69e006ee9b45abad1110bcc137f94a999fa
IMALDOWSKI <hikmal393@gmail.com>
2026-06-14 21:33:08 +0700

    Deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,18 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(at: '*'); // ← tambahkan ini
+
+    $middleware->web(append: [
+        \App\Http\Middleware\SetLocale::class,
+    ]);
+
+    $middleware->alias([
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
+    ]);
+})
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -21,4 +33,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+
+
+    
+
+        
     })->create();
+
+    
