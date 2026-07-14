638d66ded9c42c7d960bb9e372a4712b5bc59a12
IMALDOWSKI <hikmal393@gmail.com>
2026-07-14 12:14:09 +0700

    i18n laporan kasir

--- resources/views/laporan/kasir.blade.php
+++ resources/views/laporan/kasir.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">
-        <i class="fas fa-user-tie mr-2"></i>Laporan Per Kasir
+<h1 class="h3 mb-0 text-gray-800">
+        <i class="fas fa-user-tie mr-2"></i>{{ __('menu.laporan_kasir') }}
     </h1>
 </div>
 
